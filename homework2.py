b750bf78e66946f2e9d22862d6c26eac161efd98
Vasily Glazkov <vglazkoff@gmail.com>
2024-07-29 13:07:39 +0300

    Added homework2.py

---
+++ homework2.py
@@ -0,0 +1,14 @@
+# Количество выполненых ДЗ
+tasks_done = 12
+
+# Количество затраченых часов
+time_spent = 1.5
+
+# Название курса
+course_name = 'Python'
+
+# Время на одно задание
+time_per_task = time_spent / tasks_done
+
+print(f"Курс: {course_name}, всего задач: {tasks_done}, "
+      f"затрачено часов: {time_spent}, среднее время выполнения {time_per_task} часа.")
