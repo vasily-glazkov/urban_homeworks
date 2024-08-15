332e8c174c01112f2f50ab4e3b7c735e3bb59ba9
Vasily Glazkov <vglazkoff@gmail.com>
2024-08-15 21:30:54 +0200

    module 3_4 task complete

---
+++ module_3/module_3_5.py
@@ -0,0 +1,47 @@
+"""
+Задача "Рекурсивное умножение цифр":
+Напиши функцию get_multiplied_digits, которая принимает аргумент целое число number и подсчитывает произведение цифр
+этого числа.
+"""
+
+"""
+Пункты задачи:
+Напишите функцию get_multiplied_digits и параметр number в ней.
+Создайте переменную str_number и запишите строковое представление(str) числа number в неё.
+Основной задачей будет отделение первой цифры в числе: создайте переменную first и запишите в неё первый символ из 
+str_number в числовом представлении(int).
+Возвращайте значение first * get_multiplied_digits(int(str_number[1:])). Таким образом вы умножите первую цифру числа 
+на результат работы этой же функции с числом, но уже без первой цифры.
+4 пункт можно выполнить только тогда, когда длина str_number больше 1, 
+т.к. в противном случае не получиться взять срез str_number[1:].
+Если же длина str_number не больше 1, тогда вернуть оставшуюся цифру first.
+
+Стек вызовов будет выглядеть следующим образом:
+get_multiplied_digits(40203) -> 4 * get_multiplied_digits(203) -> 4 * 2 * get_multiplied_digits(3) -> 4 * 2 * 3
+"""
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+print(get_multiplied_digits(2234))  # 48
+print(get_multiplied_digits(40203))  # 24
+
+
+
+
+
+
+
+
+
+
+
+
+
