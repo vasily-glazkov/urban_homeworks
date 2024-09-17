528ea49344323d57d6eab3f1761d5b90ab04160a
Vasily Glazkov <vglazkoff@gmail.com>
2024-09-17 23:06:44 +0300

    module5hard.py completed

---
+++ module_5/module5hard.py
@@ -0,0 +1,197 @@
+"""
+Общее ТЗ:
+Реализовать классы для взаимодействия с платформой, каждый из которых будет содержать методы добавления видео, авторизации и регистрации пользователя и т.д.
+"""
+import time
+
+from streamlit import video
+
+
+# Каждый объект класса User должен обладать следующими атрибутами и методами:
+# Атрибуты: nickname(имя пользователя, строка), password(в хэшированном виде, число), age(возраст, число)
+
+class User:
+    """
+    Создаёт экземпляр класса User с атрибутами: nickname, password, age
+    """
+
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(str(password))
+        self.age = age
+
+    def __str__(self):
+        return f"User: {self.nickname}, Age: {self.age}"
+
+    def __repr__(self):
+        return f"User(nickname='{self.nickname}', age={self.age})"
+
+    def __eq__(self, other):
+        if isinstance(other, User):
+            return self.nickname == other.nickname
+        return False
+
+
+# Каждый объект класса Video должен обладать следующими атрибутами и методами:
+# Атрибуты: title(заголовок, строка), duration(продолжительность, секунды), time_now(секунда остановки (изначально 0)),
+# adult_mode(ограничение по возрасту, bool (False по умолчанию))
+
+class Video:
+    """
+    Создаёт экземпляр класса Video, с атрибутами: title, duration, time_now=0, adult_mode=False
+    """
+
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+    def __str__(self):
+        adult_str = " (18+)" if self.adult_mode else ""
+        return f"Video: {self.title}, Duration: {self.duration} seconds{adult_str}"
+
+    def __repr__(self):
+        return f"Video(title='{self.title}', duration={self.duration}, adult_mode={self.adult_mode})"
+
+    def __eq__(self, other):
+        if isinstance(other, Video):
+            return self.title == other.title
+        return False
+
+    def __contains__(self, keyword):
+        return keyword.lower() in self.title.lower()
+
+# Каждый объект класса UrTube должен обладать следующими атрибутами и методами:
+# Атрибуты: users(список объектов User), videos(список объектов Video), current_user(текущий пользователь, User)
+# Метод log_in, который принимает на вход аргументы: nickname, password и пытается найти пользователя в users с такими
+# же логином и паролем. Если такой пользователь существует, то current_user меняется на найденного. Помните, что password
+# передаётся в виде строки, а сравнивается по хэшу.
+# Метод register, который принимает три аргумента: nickname, password, age, и добавляет пользователя в список, если
+# пользователя не существует (с таким же nickname). Если существует, выводит на экран: "Пользователь {nickname}
+# уже существует". После регистрации, вход выполняется автоматически.
+# Метод log_out для сброса текущего пользователя на None.
+# Метод add, который принимает неограниченное кол-во объектов класса Video и все добавляет в videos, если с таким же
+# названием видео ещё не существует. В противном случае ничего не происходит.
+# Метод get_videos, который принимает поисковое слово и возвращает список названий всех видео, содержащих поисковое слово.
+# Следует учесть, что слово 'UrbaN' присутствует в строке 'Urban the best' (не учитывать регистр).
+# Метод watch_video, который принимает название фильма, если не находит точного совпадения(вплоть до пробела), то ничего
+# не воспроизводится, если же находит - ведётся отчёт в консоль на какой секунде ведётся просмотр. После текущее время
+# просмотра данного видео сбрасывается.
+# Для метода watch_video так же учитывайте следующие особенности:
+# Для паузы между выводами секунд воспроизведения можно использовать функцию sleep из модуля time.
+# Воспроизводить видео можно только тогда, когда пользователь вошёл в UrTube. В противном случае выводить в консоль
+# надпись: "Войдите в аккаунт, чтобы смотреть видео"
+# Если видео найдено, следует учесть, что пользователю может быть отказано в просмотре, т.к. есть ограничения 18+.
+# Должно выводиться сообщение: "Вам нет 18 лет, пожалуйста покиньте страницу"
+# После воспроизведения нужно выводить: "Конец видео"
+
+class UrTube:
+    """
+    Создаёт экземпляр класса UrTube
+    """
+
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def __str__(self):
+        return f"UrTube Platform: {len(self.users)} users, {len(self.videos)} videos"
+
+    def __repr__(self):
+        return f"UrTube(users={len(self.users)}, videos={len(self.videos)}, current_user={self.current_user})"
+
+    def log_in(self, nickname, password):
+        for user in self.users:
+            if user.nickname == nickname and user.password == hash(str(password)):
+                self.current_user = user
+                return
+        print('Неверный логин или пароль')
+
+    def register(self, nickname, password, age):
+        for user in self.users:
+            if user.nickname == nickname:
+                print(f'Пользователь {nickname} уже существует')
+                return
+        self.users.append(User(nickname, password, age))
+        self.log_in(nickname, password)
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *videos):
+        for video in videos:
+            if any(v.title == video.title for v in self.videos):
+                continue
+            self.videos.append(video)
+            print(f'Видео {video.title} добавлено')
+
+    def get_videos(self, word):
+        result = []
+        for video in self.videos:
+            if word.lower() in video.title.lower():
+                result.append(video.title)
+        return result
+
+    def watch_video(self, title):
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return
+
+        video = None
+        for v in self.videos:
+            if v.title == title:
+                video = v
+                break
+
+        if not video:
+            print('Такого видео нет')
+            return
+
+        if video.adult_mode and self.current_user.age < 18:
+            print("Вам нет 18 лет, пожалуйста покиньте страницу")
+            return
+
+        for sec in range(video.time_now, video.duration):
+            print(sec + 1, end=' ', flush=True)
+            time.sleep(1)
+        print('Конец видео')
+
+        video.time_now = 0
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user.nickname)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
+
+# Проверка работы __str__
+print(ur)
+print(ur.current_user)
+
+# Проверка поиска и использования __contains__
+print(ur.get_videos('лучший'))
+
+# Использование __eq__ для сравнения
+print(v1 == v2)  # False
