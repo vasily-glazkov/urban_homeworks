dd6cf4b5632e50208cdca69fa8704651d565193a
Vasily Glazkov <vglazkoff@gmail.com>
2024-09-22 14:43:55 +0300

    module_7_2.py completed

---
+++ module_7/module_7_2.py
@@ -0,0 +1,61 @@
+"""
+Задача "Записать и запомнить":
+Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name - название файла для записи,
+strings - список строк для записи.
+
+Функция должна:
+Записывать в файл file_name все строки из списка strings, каждая на новой строке.
+Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>),
+а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.
+
+Пример полученного словаря:
+{(1, 0): 'Text for tell.', (2, 16): 'Используйте кодировку utf-8.'}
+Где:
+1, 2 - номера записанных строк.
+0, 16 - номера байт, на которых началась запись строк.
+'Text for tell.', 'Используйте кодировку utf-8.' - сами строки.
+
+Пример результата выполнения программы:
+Пример выполняемого кода:
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
+"""
+
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, string in enumerate(strings):
+            position = file.tell()
+            file.write(string + '\n')
+            strings_positions[(i + 1, position)] = string
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+
+for elem in result.items():
+    print(elem)
+
+# Вывод на консоль:
+# ((1, 0), 'Text for tell.')
+# ((2, 15), 'Используйте кодировку utf-8.')
+# ((3, 64), 'Because there are 2 languages!')
+# ((4, 95), 'Спасибо!')
