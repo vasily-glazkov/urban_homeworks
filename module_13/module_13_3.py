ff234d52f84fb17e5b30d7c27a435e4d4ee689ec
Vasily Glazkov <vglazkoff@gmail.com>
2024-11-28 23:59:28 +0200

    added module_13_3.py

---
+++ module_13/module_13_3.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.types import Message
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
